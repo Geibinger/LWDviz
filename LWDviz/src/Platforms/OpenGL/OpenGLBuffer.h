c3be1405f216c8dce2156966f3c36308cab65818
Geibinger <jakob.friedl@gmx.at>
2020-05-02 23:01:51 +0200

    Layout abstraction

--- LWDviz/src/Platforms/OpenGL/OpenGLBuffer.h
+++ LWDviz/src/Platforms/OpenGL/OpenGLBuffer.h
@@ -8,11 +8,14 @@ namespace lw {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void bind() const;
-		virtual void unbind() const;
+		virtual void bind() const override;
+		virtual void unbind() const override;
 
+		virtual const BufferLayout& getLayout() const override { return m_layout; }
+		virtual void setLayout(const BufferLayout& layout) override { m_layout = layout; }
 	private:
 		uint32_t m_rendererID;
+		BufferLayout m_layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
